4a145edf45f6898a2ed078a8ba89af8a7ec73fe4
tomasgarceron <105751671+tomasgarceron@users.noreply.github.com>
2022-10-08 14:30:59 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 import insertHeaderAndFooter from './insertHeaderAndFooter.js'
 import {fetchProfileList} from './fetchProfile.js'
-
+import obras from '../data/obras.json' assert {type: "json"}
 const urlPerfiles = 'perfil.html'
-
+console.log(obras)
 console.log("ARTIMAÑAS :D")
 init();
 
@@ -21,3 +21,42 @@ async function init(event) {
 	// });
 	
 }
+
+const arteElectronico = document.getElementById("arteelectronico");
+const realidadAumentada = document.getElementById("realidadaumentada");
+const arteVisual = document.getElementById("artevisual");
+const videojuegos = document.getElementById("videojuegos");
+const categorias = [arteElectronico,realidadAumentada,arteVisual,videojuegos];
+
+const cuadradito = Array.from(document.getElementsByClassName("cuadradito"));
+
+cuadradito.forEach((item, i) => item.addEventListener('click', () => {  //añado evento onClick para que scrollee al hacer click en categoria.
+	categorias[i].scrollIntoView({ behavior: 'smooth' });
+})); 
+
+document.addEventListener('scroll', () => { // añado evento onScroll
+
+	let lastItem = cuadradito[0]
+
+	for (let i = 0; i < categorias.length; i++) {
+		if (
+			window.scrollY + 200  >= categorias[i].offsetTop &&  // si el offset de la ventana es mayor al offset de la primer card
+			window.scrollY  <= categorias[(i == 3) ? i : (i + 1)].offsetTop // de esa categoria y menor a la que le sigue,
+		 ) {																//	se activa la categoría
+			lastItem=cuadradito[i];
+
+			if(!lastItem.classList.contains("active")){
+
+				for (let j = 0; j < cuadradito.length; j++) {
+					cuadradito[j].previousElementSibling.className = "itemnombre";
+					cuadradito[j].className = cuadradito[j].className.replace("active", "");
+				}
+
+				lastItem.classList.add("active");
+				lastItem.previousElementSibling.classList.add("active");
+
+			};
+		 };
+	};
+});
+
